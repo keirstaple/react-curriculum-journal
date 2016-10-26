2dba8802601c38d79b819688a24215d3446a5225
Keir Staple <keirstaple@comcast.net>
2016-10-25 20:10:58 -0700

    add links to object and create Week component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './App.css';
+import Week from './Week';
 
 class App extends Component {
   constructor(props) {
@@ -7,23 +7,18 @@ class App extends Component {
     this.state = {
       title: "React Reference Guide",
       week1: [{
-        Subject:
-        Link1:
-        Link2:
-        Link3:
+        Subject: "New features of ES6",
+        Link1: "https://www.youtube.com/watch?v=BMUiFMZr7vk&list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84",
+        Link2: "https://www.youtube.com/watch?v=sjyJBL5fkp8",
+        Link3: "https://www.youtube.com/watch?v=6sQDTgOqh-I",
+        Link4: "https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.fvsezsvas"
       }],
       week2: [{
-        Subject:
-        Link1:
-        Link2:
-        Link3:
-      }],
-      week3: [{
-        Subject:
-        Link1:
-        Link2:
-        Link3:
-      }],
+        Subject: "ES6 Extended",
+        Link1: "https://www.youtube.com/watch?v=CozSF5abcTA",
+        Link2: "https://www.youtube.com/watch?v=AfWYO8t7ed4",
+        Link3: "https://www.youtube.com/watch?v=LmL0Gh193M0"
+      }]
     };
   }
   render() {
@@ -32,6 +27,9 @@ class App extends Component {
         <div className="App-header">
           <h1> {this.state.title} </h1>
         </div>
+
+        <Week />
+
       </div>
     );
   }
---
+++ src/Week.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class Week extends Component {
+
+}
+
+export default Week;
