3b6543847d25aa4c288101b96bbcb795b84a9c34
Keir Staple <keirstaple@comcast.net>
2016-10-25 19:04:45 -0700

    set up state in App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "React Reference Guide",
+      week1: [{
+        Subject:
+        Link1:
+        Link2:
+        Link3:
+      }],
+      week2: [{
+        Subject:
+        Link1:
+        Link2:
+        Link3:
+      }],
+      week3: [{
+        Subject:
+        Link1:
+        Link2:
+        Link3:
+      }],
+    };
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1> {this.state.title} </h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
