b2c86ddca7b920c841493f72b52d5ae81ba187af
Keir Staple <keirstaple@comcast.net>
2016-11-29 18:38:42 -0800

    fix issue with default optionValue missing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     this.state = {
       titleValue: '',
       linkValue: '',
-      optionValue: '',
+      optionValue: '0',
       courseWeeks: [
         {
           week: [
